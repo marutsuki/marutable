6eaef0682b2b15369dcd3003a3bebf6083726591
lucien7789 <68990764+lucien7789@users.noreply.github.com>
2024-04-13 13:53:02 +1000

    Added callbacks for interaction + removed unused prop types

---
+++ src/table/GenericTable.interaction.stories.tsx
@@ -0,0 +1,158 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import GenericTable, { GenericTableProps } from './GenericTable';
+import "./GenericTable.interaction.stories.css";
+
+type TableType = {
+  col1: string;
+  col2: string;
+  col3: boolean;
+  complex: {
+    someValue: number;
+    otherValue: number;
+  }
+}
+
+const meta = {
+  title: 'GenericTable - Interactions',
+  component: GenericTable<TableType>,
+  parameters: {
+    layout: 'fullscreen',
+  },
+} satisfies Meta<typeof GenericTable<TableType>>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+const commonProps: GenericTableProps<TableType> = {
+    className: "interactable-table",
+    cellProps: {
+        className: "interactable-cell"
+    },
+    activeColumns: ["col1", "col2", "col3", "complex"],
+    columns: {
+      col1: {
+        label: "col1",
+        getValue: (val) => val
+      },
+      col2: {
+        label: "col2",
+        getValue: (val) => val
+      },
+      col3: {
+        label: "col3",
+        getValue: (val) => String(val)
+      },
+      complex: {
+        label: "a number",
+        getValue: (val) => val.someValue * val.otherValue
+      }
+    },
+    rows: [
+      {
+        col1: {
+          value: "value1"
+        },
+        col2: {
+          value: "value2"
+        },
+        col3: {
+          value: true
+        },
+        complex: {
+          value: {
+            someValue: 3,
+            otherValue: 10,
+          }
+        }
+      },
+      {
+        col1: {
+          value: "value1_2"
+        },
+        col2: {
+          value: "value2_2"
+        },
+        col3: {
+          value: true
+        },
+      }
+    ]
+};
+
+export const AllCellsSelectable: Story = {
+  args: {
+    ...commonProps,
+    onCellSelected: (rowNo, colId, value) => alert(`Cell selected:\n rowNo: ${rowNo}\n colId: ${colId}\n value: ${JSON.stringify(value)}`),
+  }
+};
+
+export const SingleCellSelectable: Story = {
+    args: {
+      ...commonProps,
+      rows: [
+            {
+                col1: {
+                    value: "select me!!",
+                    onCellSelected: () => alert("You found me!!")
+                },
+                col2: {
+                    value: "doesnt work"
+                }
+            }
+        ],
+    }
+};
+
+export const HeadersSelectable: Story = {
+    args: {
+      ...commonProps,
+      columns: {
+        col1: {
+            label: "Selectable1",
+            getValue: () => "",
+        },
+        col2: {
+            label: "Selectable2",
+            getValue: () => "",
+        },
+        col3: {
+            label: "Selectable3",
+            getValue: () => "",
+        },
+        complex: {
+            label: "Selectable4",
+            getValue: () => "",
+        }
+      },
+      headerCellProps: {
+        className: "interactable-header",
+      },
+      onHeaderSelected: (colNo, colId, label) => alert(`You selected me!!\n colNo: ${colNo}\n colId ${colId}\n label: ${label}`),
+    }
+};
+
+export const SingleHeaderSelectable: Story = {
+    args: {
+      ...commonProps,
+      columns: {
+        col1: {
+            label: "Selectable",
+            getValue: () => "",
+            className: "interactable-header",
+            onColumnHeaderSelected: () => alert("You selected me!!"),
+        },
+        col2: {
+            label: "Unselectable",
+            getValue: () => "",
+        },
+        col3: {
+            label: "Unselectable",
+            getValue: () => "",
+        },
+        complex: {
+            label: "Unselectable",
+            getValue: () => "",
+        }
+      }
+    }
+};
--- src/table/GenericTable.tsx
+++ src/table/GenericTable.tsx
@@ -1,14 +1,17 @@
-import React, { ReactNode, useMemo } from "react";
+import React, { MouseEvent, ReactNode } from "react";
 import "./GenericTable.css";
 
 type Column<V> = {
     label: ReactNode;
     getValue: (val: V) => ReactNode;
     fixedWidth?: string;
+    onColumnHeaderSelected?: (e: MouseEvent) => void;
+    className?: string;
 };
 
-type Row<K, V> = {
+type Cell<V> = {
     value: V;
+    onCellSelected?: (e: MouseEvent) => void;
 }
 
 export type GenericTableProps<T> = {
@@ -17,36 +20,66 @@ export type GenericTableProps<T> = {
         [K in keyof T]: Column<T[K]>;
     },
     rows: {
-        [K in keyof T]?: Row<K, T[K]>;
+        [K in keyof T]?: Cell<T[K]>;
     }[];
     headerProps?: React.HTMLProps<HTMLTableRowElement>;
     rowProps?: React.HTMLProps<HTMLTableRowElement>;
     headerCellProps?: React.HTMLProps<HTMLTableCellElement>;
     cellProps?: React.HTMLProps<HTMLTableCellElement>;
+    onHeaderSelected?: (colNo: number, colId?: keyof T, label?: ReactNode) => void;
+    onCellSelected?: (rowNo: number, colId: keyof T, value?: T[keyof T], e?: MouseEvent) => void;
 } & Omit<React.HTMLProps<HTMLTableElement>, "rows" | "columns">;
 
-export default function GenericTable<T>({ activeColumns, columns, rows, className, headerProps = {}, rowProps = {}, headerCellProps = {}, cellProps = {}, ...props } : GenericTableProps<T>): ReactNode {
-    const gridTemplateColumns = useMemo(() => activeColumns.map(col => 
-        columns[col].fixedWidth || "1fr"
-    ).join(" "), [activeColumns, columns]);
-
+export default function GenericTable<T>({ 
+    activeColumns, 
+    columns, 
+    rows, 
+    className, 
+    headerProps = {}, 
+    rowProps = {}, 
+    headerCellProps = {}, 
+    cellProps = {}, 
+    onHeaderSelected,
+    onCellSelected,
+    ...props 
+} : GenericTableProps<T>): ReactNode {
     const { className: headerClassName, ...otherHeaderProps } = headerProps;
     const { className: rowClassName, ...otherRowProps } = rowProps;
     const { className: headerCellClassName, ...otherHeaderCellProps } = headerCellProps;
     const { className: cellClassName, ...otherCellProps } = cellProps;
 
+    const onColumnHeaderSelected = (e: MouseEvent<HTMLTableCellElement>, colId: keyof T) => {
+        const colHeaderCallback = columns[colId].onColumnHeaderSelected;
+        if (colHeaderCallback !== undefined) {
+            colHeaderCallback(e);
+        }
+        if (onHeaderSelected !== undefined) {
+            onHeaderSelected(activeColumns.findIndex(col => col === colId), colId, columns[colId].label);
+        }
+    }
+    
+    const onCellSelectedWrapper = (e: MouseEvent<HTMLTableCellElement>, rowNo: number, colId: keyof T) => {
+        const cellCallback = rows[rowNo][colId]?.onCellSelected;
+        if (cellCallback !== undefined) {
+            cellCallback(e);
+        }
+        if (onCellSelected !== undefined) {
+            onCellSelected(rowNo, colId, rows[rowNo][colId]?.value, e);
+        }
+    }
     return <table className={"generic-table ".concat(className || "")} {...props}>
         <tr className={"generic-table-header ".concat(headerProps.className || "")} {...otherHeaderProps}>
         {
             activeColumns.map((col, i) => 
                 <th 
                     key={`generic-table-col-${String(col)}`} 
-                    className={`generic-table-col generic-table-col-${String(col)} ${headerCellClassName}`}
+                    className={`generic-table-col generic-table-col-${String(col)} ${headerCellClassName || ""} ${columns[col].className || ""}`}
                     style={{
                         gridRow: 0,
                         gridColumn: i + 1,
                         width: columns[col].fixedWidth || "auto"
                     }}
+                    onClick={(e) => onColumnHeaderSelected(e, col)}
                     {...otherHeaderCellProps}
                 >
                     {columns[col].label}
@@ -63,6 +96,7 @@ export default function GenericTable<T>({ activeColumns, columns, rows, classNam
                             return <td 
                                 key={`generic-table-row-${String(row)}-cell-${String(col)}`} 
                                 className={`generic-table-row-${String(row)}-cell-${String(col)} ${cellClassName || ""}`}
+                                onClick={(e) => onCellSelectedWrapper(e, i, col)}
                                 {...otherCellProps}
                             >
                                 { val !== undefined && columns[col].getValue(val) }
